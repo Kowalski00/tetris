fa8488ddc43cc09712380635aca58fc928f576cf
kowalski00 <rk0191@hotmail.com>
2025-01-18 18:57:53 -0300

    Adds game loop and draw play area

---
+++ src/main/PlayManager.java
@@ -0,0 +1,45 @@
+package main;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+
+public class PlayManager {
+
+	final int WIDTH = 360;
+	final int HEIGHT = 600;
+	public static int left_x;
+	public static int right_x;
+	public static int top_y;
+	public static int bottom_y;
+	
+	public PlayManager() {
+		left_x = (GamePanel.WIDTH / 2) - (WIDTH / 2);
+		right_x = left_x + WIDTH;
+		top_y = 50;
+		bottom_y = top_y + HEIGHT;
+	}
+	
+	public void update() {
+		
+	}
+	
+	public void draw(Graphics2D graphics) {
+		graphics.setColor(Color.white);
+		graphics.setStroke(new BasicStroke(4f));
+		graphics.drawRect(left_x - 4, top_y - 4, WIDTH + 8, HEIGHT + 8);
+		drawNextTetrominoBox(graphics);
+
+	}
+	
+	private void drawNextTetrominoBox(Graphics2D graphics) {
+		int x = right_x + 100;
+		int y = bottom_y - 200;
+		graphics.drawRect(x, y, 200, 200);
+		graphics.setFont(new Font("Arial", Font.PLAIN, 30));
+		graphics.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+		graphics.drawString("Next", x + 60, y + 60);
+	}
+}
